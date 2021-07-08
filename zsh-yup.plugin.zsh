1a683f081c21bb1ba2a699da26d3e5aba4a31218
Gabe Dunn <gdunn@pipewise.ca>
2021-07-08 14:40:53 -0600

    add colours

--- zsh-yup.plugin.zsh
+++ zsh-yup.plugin.zsh
@@ -2,6 +2,10 @@
 
 # zsh plugin to update all dependencies of a yarn/npm project
 
+# load colours
+autoload -Uz colors
+colors
+
 # add the functions to the function path & autoload them
 fpath+=("$(dirname ${0})/functions")
 autoload -Uz yup
